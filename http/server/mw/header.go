53d6efabe1138e3d90edcecf08d549a98fa12099
Gaiaz Iusipov <g.iusipov@gmail.com>
2025-06-13 10:20:07 +0400

    Add `httpservermw.ContentTypeByExt`

--- http/server/mw/header.go
+++ http/server/mw/header.go
@@ -2,6 +2,7 @@ package httpservermw
 
 import (
 	"net/http"
+	"path/filepath"
 
 	httpheader "github.com/gaiaz-iusipov/go-app/http/header"
 )
@@ -17,4 +18,17 @@ func Header(key, val string) func(next http.Handler) http.Handler {
 
 func HeaderImmutable(next http.Handler) http.Handler {
 	return Header(httpheader.CacheControl, httpheader.CacheControlImmutable)(next)
+}
+
+func ContentTypeByExt(contentTypeByExt map[string]string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			ext := filepath.Ext(req.URL.Path)
+			if v, ok := contentTypeByExt[ext]; ok {
+				rw.Header().Set(httpheader.ContentType, v)
+			}
+
+			next.ServeHTTP(rw, req)
+		})
+	}
 }
