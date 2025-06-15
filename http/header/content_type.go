3ef466c51882c21da8a5342472b1731ab4ebf7a0
Gaiaz Iusipov <g.iusipov@gmail.com>
2025-06-15 15:15:48 +0400

    Add `httpheader.ContentTypeJS`

--- http/header/content_type.go
+++ http/header/content_type.go
@@ -4,4 +4,5 @@ const (
 	ContentType     = "Content-Type"
 	ContentTypeJSON = "application/json"
 	ContentTypeHTML = "text/html; charset=utf-8"
+	ContentTypeJS   = "text/javascript; charset=utf-8"
 )
