bd2636fd7ac7e0888d0aae921fca4cbc0dc44d4b
Gaiaz Iusipov <g.iusipov@gmail.com>
2025-07-06 20:26:51 +0400

    Remove redundant `httpservermw.RoutePattern`

--- http/server/mw/otel.go
+++ http/server/mw/otel.go
@@ -2,21 +2,9 @@ package httpservermw
 
 import (
 	"net/http"
-	"strings"
 
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel/trace"
 )
-
-func RoutePattern(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		if idx := strings.IndexByte(req.Pattern, '/'); idx >= 0 {
-			otelhttp.WithRouteTag(req.Pattern[idx:], next).ServeHTTP(rw, req)
-		} else {
-			next.ServeHTTP(rw, req)
-		}
-	})
-}
 
 func TraceIDHeader(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
